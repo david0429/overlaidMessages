ef615d18d33bcd71dc69063f9d4382f53f5add78
Dave Niewinski <david_0429@hotmail.com>
2019-12-03 21:52:28 -0500

    Initial commit

---
+++ overlaidMessages.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import argparse
+import random
+
+parser = argparse.ArgumentParser(description='Hides a message in multiple overlaid pages')
+parser.add_argument('--padding', type=float, default=1.5, help='How much extra character padding is added to the longest word')
+parser.add_argument('--message', type=str, default="This is an example string", help='The message to hide amongst the sheets')
+parser.add_argument('--sheets', type=int, default=5, help='Number of pages to create')
+
+def printSheets(sheets):
+    for i in range(len(sheets[0])):
+        for j in range(len(sheets)):
+            print sheets[i][j]
+        print "--------------------------------------"
+
+def getLines(line, line_len, sheets):
+    char_locations = []
+    while True:
+        r_value = random.randint(0, line_len-1)
+        if r_value not in char_locations:
+            char_locations.append(r_value)
+        if len(char_locations) == len(line):
+            break
+
+    char_locations = sorted(char_locations)
+
+    sheet_data = [ [' '] * line_len ] * sheets
+
+    for i in range(len(char_locations)):
+        sheet_data[random.randint(0, sheets-1)][char_locations[i]] = line[i]
+
+    return sheet_data
+
+def main(args):
+    message = args.message
+    message_data = message.split()
+
+    line_len = int( max(len(x) for x in message_data) * args.padding )
+
+    pages = []
+    for line in message_data:
+        pages.append(getLines(line, line_len, args.sheets))
+
+    printSheets(pages)
+
+if __name__== "__main__":
+    args = parser.parse_args()
+    main(args)
